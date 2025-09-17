40e6c4b2ac6a34c789d7d07497b1d3c91762ebb1
Rishi Rana <rishirana012354@gmail.com>
2025-09-17 10:52:52 +0530

    t

--- 01_First/pythonDjango/pythonDjango/settings.py
+++ 01_First/pythonDjango/pythonDjango/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps',
-    'tailwind'
+    'tailwind',
+    'theme',
 ]
+TAILWIND_APP_NAME= 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
