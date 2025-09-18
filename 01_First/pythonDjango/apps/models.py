20ae5f0eeb0442941b7dadccb64fe042b047db73
Rishi Rana <rishirana012354@gmail.com>
2025-09-18 23:21:26 +0530

    Model, url

--- 01_First/pythonDjango/apps/models.py
+++ 01_First/pythonDjango/apps/models.py
@@ -14,6 +14,8 @@ class Product(models.Model):
     image= models.ImageField(upload_to='products/')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=PRODUCT_TYPE_CHOICES, default='Ot')
+    description = models.TextField(default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.name
--- 01_First/pythonDjango/apps/views.py
+++ 01_First/pythonDjango/apps/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def all(request):
-    return render(request, 'apps/all.html')
+    products = Product.objects.all()
+    return render(request, 'apps/all.html', {'products': products})
