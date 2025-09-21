63f839cc56964dd20ca6d3903e9ba0079e6058fd
Rishi Rana <rishirana012354@gmail.com>
2025-09-22 00:50:53 +0530

    All done

--- PROJECT/center/center/urls.py
+++ PROJECT/center/center/urls.py
@@ -14,12 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.urls import views as auth_views
+from tweet import views as tweet_views
+
+
 
 urlpatterns = [
+    path('', tweet_views.index, name='home'),
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
-]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
